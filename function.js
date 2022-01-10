478a119c954b5a03c00c52fb040b2992377d1407
AlisonOsnir <alison_osnir@hotmail.com>
2022-01-10 00:54:10 -0300

    Form radio is working

--- function.js
+++ function.js
@@ -1,15 +1,23 @@
 function varreduraDisp() {
     for (let i = 0; i < keyList.length; i++) {
-        if (locacoesObj[i]["disp"] == false) {
+        if (locacoesObj[i]["status"]) {
             ctx2.fillStyle = 'rgba(255, 255, 0)';
             //ctx2.drawImage(canvas2, 0, 0,)
             ctx2.fillRect(...locacoesObj[i]["area"]);
             ctx2.font = 'bold 13px calibri, sans-serif';
             ctx2.fillStyle = 'black';
-            ctx2.fillText(locacoesObj[i]["produto"],
-            (locacoesObj[i]["area"][0] + 7), locacoesObj[i]["area"][1] + locacoesObj[i]["area"][3] * 0.57);
+            ctx2.fillText(locacoesObj[i]["produto"],(locacoesObj[i]["area"][0] + 7), locacoesObj[i]["area"][1] + locacoesObj[i]["area"][3] * 0.57);
         }
     }
+}
+
+/*Se varreduraDisp atuar corretamente se tornara desnessesario.*/
+function tornaIndisp(x, y, w, h, produto) {
+    ctx.fillStyle = 'rgba(255, 255, 0)';
+    ctx.fillRect(x, y, w, h);
+    ctx.font = 'bold 13px calibri, sans-serif';
+    ctx.fillStyle = 'black';
+    ctx.fillText(produto, x + 7, y + h * 0.57);
 }
 
 var areaSelecionada = null;
@@ -24,7 +32,7 @@ function selecionaArea(evento) {
             areaSelecionada = i;
 
             //Exibir locacoesObj[areaSelecinada][nome] no formulário e focar no campo produto
-            console.log(`Indice ${areaSelecionada} referente a Locaçao ${locacoesObj[areaSelecionada]["nome"]} foi selecionado!`);
+            console.log(`Indice ${areaSelecionada}, locaçao ${locacoesObj[areaSelecionada]["nome"]} foi selecionado!`);
             floc.value = areaSelecionada;
             flocname.value = locacoesObj[areaSelecionada]["nome"];
             fprod.focus();
@@ -32,11 +40,3 @@ function selecionaArea(evento) {
     }
 }
 
-/*Se varreduraDisp atuar corretamente se tornara desnessesario.*/
-function tornaIndisp(x, y, w, h, produto) {
-    ctx.fillStyle = 'rgba(255, 255, 0)';
-    ctx.fillRect(x, y, w, h);
-    ctx.font = 'bold 13px calibri, sans-serif';
-    ctx.fillStyle = 'black';
-    ctx.fillText(produto, x + 7, y + h * 0.57);
-}
