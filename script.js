3bb4b4919b234fd91b23fda2530351b54084e3ee
AlisonOsnir <alison_osnir@hotmail.com>
2022-01-14 00:30:03 -0300

    it's aliveeee

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-function varreduraDisp() {
+let indexObj = undefined;
+
+function varreduraIndispStorage() {
     for (let i = 0; i < keyList.length; i++) {
-        if (locacoesObj[i]["status"]) {
-            ctx2.fillStyle = 'rgba(255, 255, 0)';
-            //ctx2.drawImage(canvas2, 0, 0,)
-            ctx2.fillRect(...locacoesObj[i]["area"]);
-            ctx2.font = 'bold 13px calibri, sans-serif';
-            ctx2.fillStyle = 'black';
-            ctx2.fillText(locacoesObj[i]["produto"],(locacoesObj[i]["area"][0] + 7), locacoesObj[i]["area"][1] + locacoesObj[i]["area"][3] * 0.57);
+        if (localStorage.getItem(i) !== null) {
+            let retornoStorage = localStorage.getItem(i);
+            let objRetornado = JSON.parse(retornoStorage);
+            if (objRetornado['status'] == null) {
+                localStorage.removeItem(i);
+            } else {
+                tornaIndisp(...objRetornado['area'], objRetornado['produto']);
+            }
         }
     }
 }
 
-/*Se varreduraDisp() atuar corretamente se tornara desnessesario.*/
 function tornaIndisp(x, y, w, h, produto) {
-    ctx.fillStyle = 'rgba(255, 255, 0)';
-    ctx.fillRect(x, y, w, h);
-    ctx.font = 'bold 13px calibri, sans-serif';
-    ctx.fillStyle = 'black';
-    ctx.fillText(produto, x + 7, y + h * 0.57);
+    ctx2.fillStyle = 'rgba(255, 255, 0)';
+    ctx2.fillRect(x, y, w, h);
+    ctx2.font = 'bold 13px calibri, sans-serif';
+    ctx2.fillStyle = 'black';
+    ctx2.fillText(produto, x + 7, y + h * 0.57);
 }
 
-let indexObj = null;
 function selecionaArea(evento) {
     let x = evento.pageX - canvas.offsetLeft;
     let y = evento.pageY - canvas.offsetTop;
@@ -30,13 +31,19 @@ function selecionaArea(evento) {
             && y >= locacoesObj[i]["area"][1] && y <= locacoesObj[i]["area"][1] + locacoesObj[i]["area"][3]) {
 
             indexObj = i;
-
-            //Exibir locacoesObj[areaSelecinada][nome] no formulário e focar no campo produto
-            console.log(`Indice ${indexObj}, locaçao ${locacoesObj[indexObj]["nome"]} foi selecionado!`);
             floc.value = indexObj;
             flocname.value = locacoesObj[indexObj]["nome"];
             fprod.focus();
+            console.log(`Indice ${indexObj}, locaçao ${locacoesObj[indexObj]["nome"]} foi selecionado!`);
         }
     }
 }
 
+function statusRadioValue() {
+    const radio = document.getElementsByName('status');
+    for (i = 0; i < radio.length; i++) {
+        if (radio[i].checked) {
+            fstatus = radio[i].value;
+        }
+    }
+}
